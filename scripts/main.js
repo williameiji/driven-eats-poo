3c39db0d97b9d63078f7a7e1a38516db8d3be80c
williameiji <william.fujimori@gmail.com>
2022-11-19 20:16:15 -0300

    feat: add class items

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
+class Items {
+	constructor(nome, imagem, descricao, preco) {
+		this.nome = nome;
+		this.imagem = `../img/${imagem}.png`;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	getView(select) {
+		const view = document.createElement("div");
+		view.classList.add("opcao");
+		view.addEventListener("click", () => {
+			select(view, this.nome, this.preco);
+		});
+		view.innerHTML = `
+          <img src="${this.imagem}" />
+          <div class="titulo">${this.nome}</div>
+          <div class="descricao">${this.descricao}</div>
+          <div class="fundo">
+              <div class="preco">R$ ${this.preco.toFixed(2)}</div>
+              <div class="check">
+                  <ion-icon name="checkmark-circle"></ion-icon>
+              </div>
+          </div>
+      `;
+
+		return view;
+	}
+}
+
 let pratoSelecionado = null;
 let bebidaSelecionada = null;
 let sobremesaSelecionada = null;
@@ -7,244 +37,150 @@ const btnCancelar = document.querySelector(".cancelar");
 const btnPedir = document.querySelector(".fazer-pedido");
 
 const pratos = [
-  {
-    nome: "Estrombelete de Frango",
-    imagem: "img/frango_yin_yang.png",
-    descricao: "Um pouco de batata, um pouco de salada",
-    preco: 14.9,
-  },
-  {
-    nome: "Asa de Boi",
-    imagem: "img/frango_yin_yang.png",
-    descricao: "Com molho shoyu",
-    preco: 14.9,
-  },
-  {
-    nome: "Carne de Monstro",
-    imagem: "img/frango_yin_yang.png",
-    descricao: "Com batata assada e farofa",
-    preco: 14.9,
-  },
+	new Items(
+		"Estrombelete de Frango",
+		"frango_yin_yang",
+		"Um pouco de batata, um pouco de salada",
+		14.9
+	),
+	new Items("Asa de Boi", "frango_yin_yang", "Com molho shoyu", 14.9),
+	new Items(
+		"Carne de Monstro",
+		"frango_yin_yang",
+		"Com batata assada e farofa",
+		14.9
+	),
 ];
 
 const bebidas = [
-  {
-    nome: "Coquinha gelada",
-    imagem: "img/coquinha_gelada.png",
-    descricao: "Lata 350ml",
-    preco: 4.9,
-  },
-  {
-    nome: "Caldo de Cana",
-    imagem: "img/coquinha_gelada.png",
-    descricao: "Copo 600ml",
-    preco: 4.9,
-  },
-  {
-    nome: "Corote Gelado",
-    imagem: "img/coquinha_gelada.png",
-    descricao: "Garrafa 400ml",
-    preco: 4.9,
-  },
+	new Items("Coquinha gelada", "coquinha_gelada", "Lata 350ml", 4.9),
+	new Items("Caldo de Cana", "coquinha_gelada", "Copo 600ml", 4.9),
+	new Items("Corote Gelado", "coquinha_gelada", "Garrafa 400ml", 4.9),
 ];
 
 const sobremesas = [
-  {
-    nome: "Pudim",
-    imagem: "img/pudim.png",
-    descricao: "Gosto de doce de leite",
-    preco: 7.9,
-  },
-  {
-    nome: "Flam",
-    imagem: "img/pudim.png",
-    descricao: "Gosto de chocolate",
-    preco: 7.9,
-  },
-  {
-    nome: "Brigadeiro",
-    imagem: "img/pudim.png",
-    descricao: "3 unidades",
-    preco: 7.9,
-  },
+	new Items("Pudim", "pudim", "Gosto de doce de leite", 7.9),
+	new Items("Flam", "pudim", "Gosto de chocolate", 7.9),
+	new Items("Brigadeiro", "pudim", "3 unidades", 7.9),
 ];
 
 function selecionarPrato(elemento, { nome, preco }) {
-  const selecionado = document.querySelector(".prato .selecionado");
-  if (selecionado !== null) {
-    selecionado.classList.remove("selecionado");
-  }
-  elemento.classList.add("selecionado");
+	const selecionado = document.querySelector(".prato .selecionado");
+	if (selecionado !== null) {
+		selecionado.classList.remove("selecionado");
+	}
+	elemento.classList.add("selecionado");
 
-  pratoSelecionado = {
-    nome,
-    preco,
-  };
-  verificarPedido();
+	pratoSelecionado = {
+		nome,
+		preco,
+	};
+	verificarPedido();
 }
 
 function selecionarBebida(elemento, { nome, preco }) {
-  const selecionado = document.querySelector(".bebida .selecionado");
-  if (selecionado !== null) {
-    selecionado.classList.remove("selecionado");
-  }
-  elemento.classList.add("selecionado");
+	const selecionado = document.querySelector(".bebida .selecionado");
+	if (selecionado !== null) {
+		selecionado.classList.remove("selecionado");
+	}
+	elemento.classList.add("selecionado");
 
-  bebidaSelecionada = { nome, preco };
-  verificarPedido();
+	bebidaSelecionada = { nome, preco };
+	verificarPedido();
 }
 
 function selecionarSobremesa(elemento, { nome, preco }) {
-  const selecionado = document.querySelector(".sobremesa .selecionado");
-  if (selecionado !== null) {
-    selecionado.classList.remove("selecionado");
-  }
-  elemento.classList.add("selecionado");
+	const selecionado = document.querySelector(".sobremesa .selecionado");
+	if (selecionado !== null) {
+		selecionado.classList.remove("selecionado");
+	}
+	elemento.classList.add("selecionado");
 
-  sobremesaSelecionada = { nome, preco };
-  verificarPedido();
+	sobremesaSelecionada = { nome, preco };
+	verificarPedido();
 }
 
 function getPrecoTotal() {
-  return (
-    pratoSelecionado.preco +
-    bebidaSelecionada.preco +
-    sobremesaSelecionada.preco
-  );
+	return (
+		pratoSelecionado.preco +
+		bebidaSelecionada.preco +
+		sobremesaSelecionada.preco
+	);
 }
 
 function confirmarPedido() {
-  const modal = document.querySelector(".overlay");
-  modal.classList.remove("escondido");
+	const modal = document.querySelector(".overlay");
+	modal.classList.remove("escondido");
 
-  document.querySelector(".confirmar-pedido .prato .nome").innerHTML =
-    pratoSelecionado.nome;
-  document.querySelector(".confirmar-pedido .prato .preco").innerHTML =
-    pratoSelecionado.preco.toFixed(2);
+	document.querySelector(".confirmar-pedido .prato .nome").innerHTML =
+		pratoSelecionado.nome;
+	document.querySelector(".confirmar-pedido .prato .preco").innerHTML =
+		pratoSelecionado.preco.toFixed(2);
 
-  document.querySelector(".confirmar-pedido .bebida .nome").innerHTML =
-    bebidaSelecionada.nome;
-  document.querySelector(".confirmar-pedido .bebida .preco").innerHTML =
-    bebidaSelecionada.preco.toFixed(2);
+	document.querySelector(".confirmar-pedido .bebida .nome").innerHTML =
+		bebidaSelecionada.nome;
+	document.querySelector(".confirmar-pedido .bebida .preco").innerHTML =
+		bebidaSelecionada.preco.toFixed(2);
 
-  document.querySelector(".confirmar-pedido .sobremesa .nome").innerHTML =
-    sobremesaSelecionada.nome;
-  document.querySelector(".confirmar-pedido .sobremesa .preco").innerHTML =
-    sobremesaSelecionada.preco.toFixed(2);
+	document.querySelector(".confirmar-pedido .sobremesa .nome").innerHTML =
+		sobremesaSelecionada.nome;
+	document.querySelector(".confirmar-pedido .sobremesa .preco").innerHTML =
+		sobremesaSelecionada.preco.toFixed(2);
 
-  document.querySelector(".confirmar-pedido .total .preco").innerHTML =
-    getPrecoTotal().toFixed(2);
+	document.querySelector(".confirmar-pedido .total .preco").innerHTML =
+		getPrecoTotal().toFixed(2);
 }
 
 function cancelarPedido() {
-  const modal = document.querySelector(".overlay");
-  modal.classList.add("escondido");
+	const modal = document.querySelector(".overlay");
+	modal.classList.add("escondido");
 }
 
 function enviarZap() {
-  const telefoneRestaurante = 553299999999;
-  const encodedText = encodeURIComponent(
-    `Olá, gostaria de fazer o pedido: \n- Prato: ${
-      pratoSelecionado.nome
-    } \n- Bebida: ${bebidaSelecionada.nome} \n- Sobremesa: ${
-      sobremesaSelecionada.nome
-    } \nTotal: R$ ${getPrecoTotal().toFixed(2)}`
-  );
+	const telefoneRestaurante = 553299999999;
+	const encodedText = encodeURIComponent(
+		`Olá, gostaria de fazer o pedido: \n- Prato: ${
+			pratoSelecionado.nome
+		} \n- Bebida: ${bebidaSelecionada.nome} \n- Sobremesa: ${
+			sobremesaSelecionada.nome
+		} \nTotal: R$ ${getPrecoTotal().toFixed(2)}`
+	);
 
-  const urlWhatsapp = `https://wa.me/${telefoneRestaurante}?text=${encodedText}`;
-  window.open(urlWhatsapp);
+	const urlWhatsapp = `https://wa.me/${telefoneRestaurante}?text=${encodedText}`;
+	window.open(urlWhatsapp);
 }
 
 function verificarPedido() {
-  if (pratoSelecionado && bebidaSelecionada && sobremesaSelecionada) {
-    btnPedir.classList.add("ativo");
-    btnPedir.disabled = false;
-    btnPedir.innerHTML = "Fazer pedido";
-  }
-}
-
-function getPratoView(prato) {
-  const view = document.createElement("div");
-  view.classList.add("opcao");
-  view.addEventListener("click", () => {
-    selecionarPrato(view, prato.nome, prato.preco);
-  });
-  view.innerHTML = `
-        <img src="${prato.imagem}" />
-        <div class="titulo">${prato.nome}</div>
-        <div class="descricao">${prato.descricao}</div>
-        <div class="fundo">
-            <div class="preco">R$ ${prato.preco.toFixed(2)}</div>
-            <div class="check">
-                <ion-icon name="checkmark-circle"></ion-icon>
-            </div>
-        </div>
-    `;
-
-  return view;
-}
-
-function getBebidaView(bebida) {
-  const view = document.createElement("div");
-  view.classList.add("opcao");
-  view.addEventListener("click", () => {
-    selecionarBebida(view, bebida.nome, bebida.preco);
-  });
-  view.innerHTML = `
-        <img src="${bebida.imagem}" />
-        <div class="titulo">${bebida.nome}</div>
-        <div class="descricao">${bebida.descricao}</div>
-        <div class="fundo">
-            <div class="preco">R$ ${bebida.preco.toFixed(2)}</div>
-            <div class="check">
-                <ion-icon name="checkmark-circle"></ion-icon>
-            </div>
-        </div>
-    `;
-
-  return view;
-}
-
-function getSobremesaView(sobremesa) {
-  const view = document.createElement("div");
-  view.classList.add("opcao");
-  view.addEventListener("click", () => {
-    selecionarSobremesa(view, sobremesa.nome, sobremesa.preco);
-  });
-  view.innerHTML = `
-        <img src="${sobremesa.imagem}" />
-        <div class="titulo">${sobremesa.nome}</div>
-        <div class="descricao">${sobremesa.descricao}</div>
-        <div class="fundo">
-            <div class="preco">R$ ${sobremesa.preco.toFixed(2)}</div>
-            <div class="check">
-                <ion-icon name="checkmark-circle"></ion-icon>
-            </div>
-        </div>
-    `;
-
-  return view;
+	if (pratoSelecionado && bebidaSelecionada && sobremesaSelecionada) {
+		btnPedir.classList.add("ativo");
+		btnPedir.disabled = false;
+		btnPedir.innerHTML = "Fazer pedido";
+	}
 }
 
 const pratosContainer = document.querySelector(".opcoes.prato");
-pratos.forEach((prato) => pratosContainer.appendChild(getPratoView(prato)));
+pratos.forEach((prato) =>
+	pratosContainer.appendChild(prato.getView(selecionarPrato))
+);
+
 const bebidasContainer = document.querySelector(".opcoes.bebida");
 bebidas.forEach((bebida) =>
-  bebidasContainer.appendChild(getBebidaView(bebida))
+	bebidasContainer.appendChild(bebida.getView(selecionarBebida))
 );
+
 const sobremesasContainer = document.querySelector(".opcoes.sobremesa");
 sobremesas.forEach((sobremesa) =>
-  sobremesasContainer.appendChild(getSobremesaView(sobremesa))
+	sobremesasContainer.appendChild(sobremesa.getView(selecionarSobremesa))
 );
 
 btnConfirmar.addEventListener("click", () => {
-  enviarZap();
+	enviarZap();
 });
 
 btnCancelar.addEventListener("click", () => {
-  cancelarPedido();
+	cancelarPedido();
 });
 
 btnPedir.addEventListener("click", () => {
-  confirmarPedido();
+	confirmarPedido();
 });
